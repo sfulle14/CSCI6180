2dbcddc7e26645d3a5ab727ce095442ea9db4425
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2024-10-22 15:14:47 -0500

    Delete website directory

--- website/website/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic.base import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-
-
-class ProfileView(LoginRequiredMixin, TemplateView):
-    template_name = 'accounts/profile.html'
-    
