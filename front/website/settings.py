c530b4976c4cb09f939a714979b47b76e65462ce
Steven Fuller <steven.fuller96@gmail.com>
2024-10-24 11:48:47 -0500

    static files working

--- front/website/settings.py
+++ front/website/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
